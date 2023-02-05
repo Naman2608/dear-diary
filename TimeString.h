9ea453ae74ba1f0666e808f4246576130e3acf59
Ashu Sharma <mrmrashusharma@gmail.com>
2023-02-05 18:19:55 +0530

    Concatenated and converted dateString dateString ready to use

--- TimeString.h
+++ TimeString.h
@@ -6,6 +6,9 @@
 
 #endif // DEAREST_DIARY_TIMESTRING_H
 
+int DD,MM,YY;
+char DD_Str[20];
+
 // Get Time String
 void getTheTime()
 {
@@ -16,20 +19,6 @@ void getTheTime()
     MM = tm.tm_mon+1, // Here 0 = January and 11 = December, so an increment is made to get the dateString
     YY = tm.tm_year+1900; // This returns the total number of years since 1900, so adding 1900 will get the current dateString
 
-    sprintf(DD_Str, "%d", DD);
-    sprintf(MM_Str, "%d", MM);
-    sprintf(YY_Str, "%d", YY);
-
-    printf("%s-%s-%s", DD_Str, MM_Str, YY_Str);
-}
-
-
-// Concatenate to Time String
-char* concat(const char *s1, const char *s2)
-{
-    char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
-    // in real code you would check for errors in malloc here
-    strcpy(result, s1);
-    strcat(result, s2);
-    return result;
+    sprintf(DD_Str, "%d-%d-%d", DD, MM, YY);
+    printf("%s", DD_Str);
 }
--- exps.c
+++ exps.c
@@ -5,9 +5,6 @@
 #include <stdio.h>
 #include <time.h>
 
-int DD,MM,YY;
-char DD_Str[2], MM_Str[2], YY_Str[4];
-
 #include "TimeString.h"
 
 int main(void) {
