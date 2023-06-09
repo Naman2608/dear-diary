3ab25e2b05c09ebe4b277a7133496227b4c4aa9e
mrmrashu <mrmrashusharma@gmail.com>
2023-06-09 18:43:23 +0530

    Fixed the Segmentation Fault!

--- main.c
+++ main.c
@@ -139,7 +139,7 @@ void add_entry()
 // Opening Diary
 void open_Diary(void)
 {
-	char *file_name;
+	char *file_name = malloc(sizeof(char) * 20);
 	printf("Enter the Date for Dairy Entry in the following format :  DD-MM-YY \n for eg:(23-4-2023) \n");
 	scanf("%s", file_name);
 
@@ -147,6 +147,7 @@ void open_Diary(void)
 	if (fp == NULL)
 	{
 		printf("No Dairy exist");
+		free(file_name);
 		exit(1);
 	}
 	else
@@ -158,6 +159,7 @@ void open_Diary(void)
 		}
 	}
 	fclose(fp);
+	free(file_name);
 }
 // Displaying Help Menu
 void help_menu()
@@ -173,47 +175,3 @@ void invalid_args()
 {
 	printf("Invalid Argument(s), Please use '-h' for more information\n");
 }
-
-/*int main(int argc, char const *argv[])
-{
-	int tm_isdst; // Daylight Savings Time flag
-	getTheTime();
-	// printf("THE TIME IS : %s\n", TT_Str);
-	// int i = 0;
-	if (argc > 1)
-	{
-		printf("%d Argument(s) Recieved,\n", argc - 1); // Checking the Arguments
-		for (int i = 1; i < argc; i++)
-		{
-			printf("Argument %d : %s\n", i, argv[i]);
-			// Redirecting to help menu
-			if (!strcmp(argv[i], "-h"))
-			{
-				help_menu();
-			}
-			// Redirecting to New Entry
-			else if (!strcmp(argv[i], "-new"))
-			{
-				add_entry();
-			}
-			// Opening the diary
-			else if (!strcmp(argv[i], "-o"))
-			{
-				open_Diary();
-			}
-			// Handling any other argument entered other than above
-			else
-			{
-				invalid_args();
-			}
-		}
-	}
-	// Handling no arguments passed
-	else
-	{
-		help_menu();
-		printf("\n---------------------------------------------\nPlease use '-new' to create a new Diary Entry\n---------------------------------------------\n");
-	}
-	return 0;
-}
-*/
